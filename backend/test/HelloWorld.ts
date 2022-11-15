9bb1fcf91c7ff2fdb876f65d8cc5692eb0f09674
hevelius <perrotta.mr@gmail.com>
2022-11-15 10:01:56 +0100

    test: update

--- backend/test/HelloWorld.ts
+++ backend/test/HelloWorld.ts
@@ -1,5 +1,4 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
@@ -8,7 +7,6 @@ describe("HelloWorld", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployHelloWorld() {
-
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
     const helloWorld = await HelloWorld.deploy();
 
@@ -16,13 +14,15 @@ describe("HelloWorld", function () {
   }
 
   describe("Deployment", function () {
-    /*it("should assert true", async () => {
-      const { helloWorld } = await loadFixture(deployHelloWorld);
-      expect(await helloWorld.deployed()).to.equal(true);
-    });*/
     it("should returns HelloWorld string", async () => {
       const { helloWorld } = await loadFixture(deployHelloWorld);
       expect(await helloWorld.getHelloWorld()).equal("HelloWorld");
-    })
+    });
+    it("should setHelloWorld with string Hello Universe", async () => {
+      const { helloWorld } = await loadFixture(deployHelloWorld);
+      const helloUniverse = "Hello Universe";
+      await helloWorld.setHelloWorld(helloUniverse);
+      expect(await helloWorld.getHelloWorld()).equal("Hello Universe");
+    });
   });
 });
